67e9e51452b772ae95b181d66e670f7bac8559f8
griyen <133221657+griyen@users.noreply.github.com>
2023-10-13 09:54:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,192 @@
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+
+const quizArray = [
+    {
+        id: "0",
+        question: "From which language is the word ketchup derived?",
+        options: [
+            "Korean",
+            "Chinese",
+            "English",
+            "None of these",
+        ],
+        correct: "Chinese",
+    },
+    {
+        id: "1",
+        question: "H2O is the chemical formula for what?",
+        options: [
+            "Oil",
+            "Metal",
+            "Water",
+            "None of these",
+        ],
+        correct: "Water",
+    },
+    {
+        id: "2",
+        question: "1+1",
+        options: [
+            "2",
+            "11",
+            "Magellan",
+            "Equals",
+        ],
+        correct: "2",
+    },
+    {
+        id: "3",
+        question: "What does HTML stand for?",
+         options: [
+            "Hyper Text Markup Language",
+            "High Technology Modern Language",
+            "Hyperlink and Text Markup Language",
+            "Home Tool Markup Language",
+        ],
+        correct: "Hyper Text Markup Language",
+    },
+    {
+        id: "4",
+        question: "Which HTML attribute is used to define the alternative text for an image?",
+        options: [
+            "alt",
+            "Title",
+            "src",
+            "href",
+        ],
+        correct: "alt",
+    },
+];
+    
+
+
+restart.addEventListener("click", () => {
+    initial();
+    displayContainer.classList.remove("hide");
+    scoreContainer.classList.add("hide")
+});
+
+nextBtn.addEventListener("click",(displayNext = () => {
+    questionCount +=1;
+
+    if(questionCount == quizArray.length){
+        displayContainer.classList.add("hide");
+        scoreContainer.classList.remove("hide")
+        userScore.innerHTML = "Your Score is " +
+        scoreCount + " out of " + questionCount;
+    }
+    else{
+        countOfQuestion.innerHTML = questionCount + 1 + " of " + quizArray.length + "Question";
+        quizDisplay(questionCount);
+        count = 11;
+        clearInterval(countdown);
+        timerDisplay();
+    }
+}));
+
+const timerDisplay = () => {
+    countdown = setInterval(()=> {
+        count --;
+        timeLeft.innerHTML =  `${count}s`;
+        if (count == 0) {
+            clearInterval(countdown);
+            displayNext();  
+        }
+    }, 1000);
+};
+
+const quizDisplay = (questionCount) => {
+    let quizCards = document.querySelectorAll(".container-mid");
+
+    quizCards.forEach((card) => {
+        card.classList.add("hide");
+    });
+    quizCards[questionCount].classList.remove("hide");
+};
+
+function quizCreater(){
+    quizArray.sort(() => Math.random () - 0.5);
+
+    for(let i of quizArray){
+        i.options.sort(()=> Math.random () - 0.5);
+        let div = document.createElement("div");
+        div.classList.add("container-mid","hide");
+
+        countOfQuestion.innerHTML = 1 + " of " + quizArray.length + "Question";
+
+        let question_DIV = document.createElement("p");
+        question_DIV.classList.add("question");
+        question_DIV.innerHTML = i.question;
+        div.appendChild(question_DIV);
+
+        div.innerHTML += `
+        <button class = "option-div" onclick="checker(this)">${i.options[0]}</button>
+        <button class = "option-div" onclick="checker(this)">${i.options[1]}</button>
+        <button class = "option-div" onclick="checker(this)">${i.options[2]}</button>
+        <button class = "option-div" onclick="checker(this)">${i.options[3]}</button>
+        `;
+
+        quizContainer.appendChild(div);
+
+    }
+}
+
+function checker(userOption){
+    let userSolution = userOption.innerText;
+    let question = document.getElementsByClassName("container-mid")[questionCount];
+    let options = question.querySelectorAll(".option-div");
+
+    if(userSolution === quizArray[questionCount].correct){
+        userOption.classList.add("correct");
+        scoreCount++;
+    } else {
+        userOption.classList.add("incorrect");
+
+        options.forEach((element) => {
+            if (element.innerText == quizArray[questionCount].correct)
+            {
+                element.classList.add("correct");
+            }
+        });
+    }
+    clearInterval(countdown);
+    options.forEach((element) => {
+        element.disabled = true;
+    });
+}
+
+function initial(){
+    quizContainer.innerHTML = "";
+    questionCount = 0;
+    scoreCount = 0;
+    scoreCount = 0;
+    count = 11;
+    clearInterval(countdown);
+    timerDisplay();
+    quizCreater();
+    quizDisplay(questionCount);
+}
+
+startButton.addEventListener("click",() => {
+    startScreen.classList.add("hide");
+    displayContainer.classList.remove("hide");
+    initial();
+});
+
+window.onload = () => {
+    startScreen.classList.remove("hide")
+    displayContainer.classList.add("hide")
+};
